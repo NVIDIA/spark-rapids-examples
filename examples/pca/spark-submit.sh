1d1833e24783efbe28b6ed208733f5e99610cefa
Allen Xu <allxu@nvidia.com>
2021-09-30 18:36:48 +0800

    Update version

--- examples/pca/spark-submit.sh
+++ examples/pca/spark-submit.sh
@@ -5,13 +5,13 @@ $SPARK_HOME/bin/spark-submit \
 --driver-memory 30G          \
 --executor-memory 30G          \
 --conf spark.driver.maxResultSize=8G          \
---conf spark.executor.extraClassPath=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/1.0-SNAPSHOT/rapids-4-spark-ml_2.12-1.0-SNAPSHOT.jar \
+--conf spark.executor.extraClassPath=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/21.10.0-SNAPSHOT/rapids-4-spark-ml_2.12-21.10.0-SNAPSHOT.jar \
 --conf spark.task.resource.gpu.amount=0.08        \
---conf spark.driver.extraClassPath=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/1.0-SNAPSHOT/rapids-4-spark-ml_2.12-1.0-SNAPSHOT.jar \
+--conf spark.driver.extraClassPath=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/21.10.0-SNAPSHOT/rapids-4-spark-ml_2.12-21.10.0-SNAPSHOT.jar \
 --conf spark.executor.resource.gpu.amount=1  \
 --conf spark.rpc.message.maxSize=2046 \
 --conf spark.executor.heartbeatInterval=500s \
 --conf spark.network.timeout=1000s \
---jars /root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/1.0-SNAPSHOT/rapids-4-spark-ml_2.12-1.0-SNAPSHOT.jar \
+--jars /root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/21.10.0-SNAPSHOT/rapids-4-spark-ml_2.12-21.10.0-SNAPSHOT.jar \
 --class com.nvidia.spark.examples.pca.Main \
-/workspace/target/PCAExample-1.0-SNAPSHOT.jar
+/workspace/target/PCAExample-21.10.0-SNAPSHOT.jar
