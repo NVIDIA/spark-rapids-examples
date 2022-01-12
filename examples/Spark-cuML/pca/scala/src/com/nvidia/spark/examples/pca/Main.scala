438aa1b3d9e241f2bc71a35d42f68c7abb3ba503
Allen Xu <wjxiz1992@gmail.com>
2022-01-12 13:47:46 +0800

    Fix input col (#84)
    Signed-off-by: Allen Xu <allxu@nvidia.com>
    
    Co-authored-by: Allen Xu <allxu@nvidia.com>

--- examples/Spark-cuML/pca/scala/src/com/nvidia/spark/examples/pca/Main.scala
+++ examples/Spark-cuML/pca/scala/src/com/nvidia/spark/examples/pca/Main.scala
@@ -38,17 +38,17 @@ object Main {
     val pcaModelGpu = pcaGpu.fit(dataDf)
     val gpuEnd = System.currentTimeMillis()
 
-    // use original Spark ML PCA class
-    val pcaCpu = new org.apache.spark.ml.feature.PCA().setInputCol("feature").setOutputCol("pca_features").setK(3)
-
     // use udf to meet standard CPU ML algo input requirement: Vector input
     val convertToVector = udf((array: Seq[Double]) => {
       Vectors.dense(array.map(_.toDouble).toArray)
     })
 
     val vectorDf = dataDf.withColumn("feature_vec", convertToVector(col("feature")))
+
+    // use original Spark ML PCA class
+    val pcaCpu = new org.apache.spark.ml.feature.PCA().setInputCol("feature_vec").setOutputCol("pca_features").setK(3)
+
     // CPU train
-    
     val cpuStart = System.currentTimeMillis()
     val pcaModelCpu = pcaCpu.fit(vectorDf)
     val cpuEnd = System.currentTimeMillis()
