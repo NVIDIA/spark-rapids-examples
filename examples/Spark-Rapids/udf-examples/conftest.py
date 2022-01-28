689e9502ed62ddc7c7bfebc2fd69fdcd9b517354
Chong Gao <res_life@163.com>
2022-01-28 18:00:11 +0800

    Add test cases; Update the Copyrights

---
+++ examples/Spark-Rapids/udf-examples/conftest.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2020-2022, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+def pytest_addoption(parser):
+    """Pytest hook to define command line options for pytest"""
+    parser.addoption(
+        "--mortgage_format", action="store", default="parquet", help="format of Mortgage data"
+    )
+    parser.addoption(
+        "--mortgage_path", action="store", default=None, help="path to Mortgage data"
+    )
+    parser.addoption(
+        "--std_input_path", action="store", default=None, help="path to standard input files"
+    )
+    parser.addoption(
+        "--tmp_path", action="store", default=None, help="path to store tmp files"
+    )
+    parser.addoption(
+        "--debug_tmp_path", action='store_true', default=False, help="if true don't delete tmp_path contents for debugging"
+    )
+    parser.addoption(
+        "--runtime_env", action='store', default="Apache", help="the runtime environment for the tests - apache or databricks"
+    )
+    parser.addoption(
+        "--cudf_udf", action='store_true', default=False, help="if true enable cudf_udf test"
+    )
+    parser.addoption(
+        "--rapids_udf_example_native", action='store_true', default=False,
+        help="if true enable tests for RAPIDS UDF examples with native code"
+    )
+    parser.addoption(
+        "--test_type", action='store', default="developer",
+        help="the type of tests that are being run to help check all the correct tests are run - developer, pre-commit, or nightly"
+    )
---
+++ examples/Spark-Rapids/udf-examples/src/main/python/spark_session.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2020-2022, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from conftest import is_allowing_any_non_gpu, get_non_gpu_allowed, get_validate_execs_in_gpu_plan, is_databricks_runtime
+from pyspark.sql import SparkSession, DataFrame
+from spark_init_internal import get_spark_i_know_what_i_am_doing, spark_version
+
+def _from_scala_map(scala_map):
+    ret = {}
+    # The value we get is a scala map, not a java map, so we need to jump through some hoops
+    keys = scala_map.keys().iterator()
+    while keys.hasNext():
+        key = keys.next()
+        ret[key] = scala_map.get(key).get()
+    return ret
+
+_spark = get_spark_i_know_what_i_am_doing()
+# Have to reach into a private member to get access to the API we need
+_orig_conf = _from_scala_map(_spark.conf._jconf.getAll())
+_orig_conf_keys = _orig_conf.keys()
+
+def is_tz_utc(spark=_spark):
+    """
+    true if the tz is UTC else false
+    """
+    # Now we have to do some kind of ugly internal java stuff
+    jvm = spark.sparkContext._jvm
+    utc = jvm.java.time.ZoneId.of('UTC').normalized()
+    sys_tz = jvm.java.time.ZoneId.systemDefault().normalized()
+    return utc == sys_tz
+
+def _set_all_confs(conf):
+    for key, value in conf.items():
+        if _spark.conf.get(key, None) != value:
+            _spark.conf.set(key, value)
+
+def reset_spark_session_conf():
+    """Reset all of the configs for a given spark session."""
+    _set_all_confs(_orig_conf)
+    #We should clear the cache
+    _spark.catalog.clearCache()
+    # Have to reach into a private member to get access to the API we need
+    current_keys = _from_scala_map(_spark.conf._jconf.getAll()).keys()
+    for key in current_keys:
+        if key not in _orig_conf_keys:
+            _spark.conf.unset(key)
+
+def _check_for_proper_return_values(something):
+    """We don't want to return an DataFrame or Dataset from a with_spark_session. You will not get what you expect"""
+    if (isinstance(something, DataFrame)):
+        raise RuntimeError("You should never return a DataFrame from a with_*_session, you will not get the results that you expect")
+
+def with_spark_session(func, conf={}):
+    """Run func that takes a spark session as input with the given configs set."""
+    reset_spark_session_conf()
+    _add_job_description(conf)
+    _set_all_confs(conf)
+    ret = func(_spark)
+    _check_for_proper_return_values(ret)
+    return ret
+
+
+def _add_job_description(conf):
+    is_gpu_job = conf.get('spark.rapids.sql.enabled', False)
+    job_type = 'GPU' if str(is_gpu_job).lower() == str(True).lower() else 'CPU'
+    job_desc = '{}[{}]'.format(os.environ.get('PYTEST_CURRENT_TEST'), job_type)
+    _spark.sparkContext.setJobDescription(job_desc)
+
+
+def with_cpu_session(func, conf={}):
+    """Run func that takes a spark session as input with the given configs set on the CPU."""
+    copy = dict(conf)
+    copy['spark.rapids.sql.enabled'] = 'false'
+    return with_spark_session(func, conf=copy)
+
+def with_gpu_session(func, conf={}):
+    """
+    Run func that takes a spark session as input with the given configs set on the GPU.
+    Note that this forces you into test mode unless.  It is not a requirement, but is
+    simplest for right now.
+    """
+    copy = dict(conf)
+    copy['spark.rapids.sql.enabled'] = 'true'
+    if is_allowing_any_non_gpu():
+        copy['spark.rapids.sql.test.enabled'] = 'false'
+    else:
+        copy['spark.rapids.sql.test.enabled'] = 'true'
+        copy['spark.rapids.sql.test.allowedNonGpu'] = ','.join(get_non_gpu_allowed())
+
+    copy['spark.rapids.sql.test.validateExecsInGpuPlan'] = ','.join(get_validate_execs_in_gpu_plan())
+    return with_spark_session(func, conf=copy)
+
+def is_before_spark_311():
+    return spark_version() < "3.1.0"
+
+def is_before_spark_320():
+    return spark_version() < "3.2.0"
+
+def is_before_spark_330():
+    return spark_version() < "3.3.0"
+
+def is_databricks91_or_later():
+    spark = get_spark_i_know_what_i_am_doing()
+    return spark.conf.get("spark.databricks.clusterUsageTags.sparkVersion", "") >= "9.1"
