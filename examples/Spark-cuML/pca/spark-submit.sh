427bacd6c952df0c48a987f9d41c00fe654ccdd6
Allen Xu <allxu@nvidia.com>
2022-02-09 10:32:42 +0800

    fix
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- examples/Spark-cuML/pca/spark-submit.sh
+++ examples/Spark-cuML/pca/spark-submit.sh
@@ -16,8 +16,8 @@
 #
 
 ML_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/22.02.0-SNAPSHOT/rapids-4-spark-ml_2.12-22.02.0-SNAPSHOT.jar
-CUDF_JAR=/root/.m2/repository/ai/rapids/cudf/22.02.0-SNAPSHOT/cudf-22.02.0-SNAPSHOT.jar
-PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/22.02.0-SNAPSHOT/rapids-4-spark_2.12-22.02.0-SNAPSHOT.jar
+CUDF_JAR=/root/.m2/repository/ai/rapids/cudf/22.02.0/cudf-22.02.0.jar
+PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/22.02.0/rapids-4-spark_2.12-22.02.0.jar
 
 $SPARK_HOME/bin/spark-submit \
 --master spark://127.0.0.1:7077  \
@@ -40,4 +40,4 @@ $SPARK_HOME/bin/spark-submit \
 --conf spark.network.timeout=1000s \
 --jars $ML_JAR,$CUDF_JAR,$PLUGIN_JAR \
 --class com.nvidia.spark.examples.pca.Main \
-/workspace/target/PCAExample-22.02.0-SNAPSHOT.jar
+/workspace/target/PCAExample-22.02.0.jar
