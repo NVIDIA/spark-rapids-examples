5482d26a7a3b0e051b47451291d89360e708d4ae
liyuan <84758614+nvliyuan@users.noreply.github.com>
2023-03-15 12:13:04 +0800

    update the dependency version to v23.04 (#283)
    * update the dependency version
    
    Signed-off-by: liyuan <yuali@nvidia.com>
    
    * upadate version
    
    Signed-off-by: liyuan <yuali@nvidia.com>
    
    ---------
    
    Signed-off-by: liyuan <yuali@nvidia.com>

--- examples/ML+DL-Examples/Spark-cuML/pca/spark-submit.sh
+++ examples/ML+DL-Examples/Spark-cuML/pca/spark-submit.sh
@@ -15,8 +15,8 @@
 # limitations under the License.
 #
 
-ML_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/23.02.0-SNAPSHOT/rapids-4-spark-ml_2.12-23.02.0-SNAPSHOT.jar
-PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/23.02.0-SNAPSHOT/rapids-4-spark_2.12-23.02.0-SNAPSHOT.jar
+ML_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/23.04.0-SNAPSHOT/rapids-4-spark-ml_2.12-23.04.0-SNAPSHOT.jar
+PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/23.04.0-SNAPSHOT/rapids-4-spark_2.12-23.04.0-SNAPSHOT.jar
 
 $SPARK_HOME/bin/spark-submit \
 --master spark://127.0.0.1:7077  \
@@ -38,4 +38,4 @@ $SPARK_HOME/bin/spark-submit \
 --conf spark.network.timeout=1000s \
 --jars $ML_JAR,$PLUGIN_JAR \
 --class com.nvidia.spark.examples.pca.Main \
-/workspace/target/PCAExample-23.02.0-SNAPSHOT.jar
+/workspace/target/PCAExample-23.04.0-SNAPSHOT.jar
