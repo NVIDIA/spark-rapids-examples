96fe911af7a090dae1dbb9d916acbb4cbd72f510
liyuan <84758614+nvliyuan@users.noreply.github.com>
2024-06-27 15:42:58 +0800

    fix version issue (#398)
    Signed-off-by: liyuan <yuali@nvidia.com>

--- examples/ML+DL-Examples/Spark-cuML/pca/spark-submit.sh
+++ examples/ML+DL-Examples/Spark-cuML/pca/spark-submit.sh
@@ -17,7 +17,7 @@
 
 # Note that the last rapids-4-spark-ml release version is 22.02.0, snapshot version is 23.04.0-SNAPSHOT, please do not update the version in release
 ML_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark-ml_2.12/22.02.0/rapids-4-spark-ml_2.12-22.02.0.jar
-PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/24.06.0/rapids-4-spark_2.12-24.06.0.jar
+PLUGIN_JAR=/root/.m2/repository/com/nvidia/rapids-4-spark_2.12/23.04.0/rapids-4-spark_2.12-23.04.0.jar
 Note: The last rapids-4-spark-ml release version is 22.02.0, snapshot version is 23.04.0-SNAPSHOT.
 
 $SPARK_HOME/bin/spark-submit \
@@ -40,4 +40,4 @@ $SPARK_HOME/bin/spark-submit \
 --conf spark.network.timeout=1000s \
 --jars $ML_JAR,$PLUGIN_JAR \
 --class com.nvidia.spark.examples.pca.Main \
-/workspace/target/PCAExample-24.06.0-SNAPSHOT.jar
+/workspace/target/PCAExample-23.04.0-SNAPSHOT.jar
