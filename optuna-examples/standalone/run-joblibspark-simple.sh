665d8f6b58d424c857e43cc63306739b9432cd6f
Rishi Chandra <rishic@nvidia.com>
2024-11-01 17:55:13 -0700

    Replace RDD example with Dataframe example, cleanups to README and repo structure

--- optuna-examples/standalone/run-joblibspark-simple.sh
+++ optuna-examples/standalone/run-joblibspark-simple.sh
@@ -1,15 +1,12 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
-export PYSPARK_PYTHON=./environment/bin/python
-
 $SPARK_HOME/bin/spark-submit \
   --master spark://$(hostname):7077 \
   --conf spark.executor.cores=8 \
-  --conf spark.executor.resource.gpu.amount=1 \
-  --conf spark.task.resource.gpu.amount=1 \
   --conf spark.executor.memory=16g \
+  --conf spark.executorEnv.PYSPARK_PYTHON=./environment/bin/python \
   --archives ../optuna-env.tar.gz#environment \
-  ../optuna-mysql-xgboost-spark.py \
-    --filepath /path/to/winequality-red.csv \
+  ../joblibspark-simple.py \
     --tasks 2 \
     --jobs 2 \
+    --localhost \
