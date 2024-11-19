0a59ceff8a2523b65102a65664adbd972b8ee90c
Rishi C. <77904151+rishic3@users.noreply.github.com>
2024-11-19 10:26:39 -0800

    Update run-joblibspark-simple.sh

--- examples/ML+DL-Examples/Optuna-Spark/optuna-examples/standalone/run-joblibspark-simple.sh
+++ examples/ML+DL-Examples/Optuna-Spark/optuna-examples/standalone/run-joblibspark-simple.sh
@@ -1,5 +1,10 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
+if [[ -z $SPARK_HOME ]]; then
+  echo "Please export SPARK_HOME to the Spark path"
+  exit 1
+fi
+
 $SPARK_HOME/bin/spark-submit \
   --master spark://$(hostname):7077 \
   --conf spark.executor.cores=8 \
