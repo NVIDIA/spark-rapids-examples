6d96bcdad15e82880121c98b76376f003ea12a99
Rishi Chandra <rishic@nvidia.com>
2025-01-10 20:53:32 +0000

    typo fix

--- examples/ML+DL-Examples/Spark-DL/dl_inference/dataproc/setup/start_cluster.sh
+++ examples/ML+DL-Examples/Spark-DL/dl_inference/dataproc/setup/start_cluster.sh
@@ -7,6 +7,11 @@ if [[ -z ${GCS_BUCKET} ]]; then
     exit 1
 fi
 
+if [[ -z ${FRAMEWORK} ]]; then
+    echo "Please export FRAMEWORK as 'torch' or 'tf'"
+    exit 1
+fi
+
 if [[ -z ${COMPUTE_REGION} ]]; then
     COMPUTE_REGION=$(gcloud config get-value compute/region)
     if [[ -z ${COMPUTE_REGION} ]]; then
@@ -20,6 +25,13 @@ SPARK_DL_HOME=${SPARK_DL_HOME:-${GCS_BUCKET}/spark-dl}
 # copy init script to gcs
 gcloud storage cp init_spark_dl.sh gs://${SPARK_DL_HOME}/init/
 INIT_PATH=gs://${SPARK_DL_HOME}/init/init_spark_dl.sh
+
+# retrieve and upload spark-rapids initialization script to gcs
+curl -LO https://raw.githubusercontent.com/GoogleCloudDataproc/initialization-actions/master/spark-rapids/spark-rapids.sh
+# don't enable rapids plugin by default
+sed -i '/spark.plugins=com.nvidia.spark.SQLPlugin/d' spark-rapids.sh
+gcloud storage cp spark-rapids.sh gs://${SPARK_DL_HOME}/init/
+# rm spark-rapids.sh
 
 COMMON_REQUIREMENTS="numpy
 pandas
@@ -54,7 +66,7 @@ if [[ ${FRAMEWORK} == "torch" ]]; then
     echo "Starting PyTorch cluster ${cluster_name}"
     echo "========================================================="
 elif [[ ${FRAMEWORK} == "tf" ]]; then
-    requirements=${TORCH_REQUIREMENTS}
+    requirements=${TF_REQUIREMENTS}
     echo "========================================================="
     echo "Starting Tensorflow cluster ${cluster_name}"
     echo "========================================================="
@@ -63,16 +75,8 @@ else
     exit 1
 fi
 
-# retrieve and upload spark-rapids initialization script to gcs
-curl -LO https://raw.githubusercontent.com/GoogleCloudDataproc/initialization-actions/master/spark-rapids/spark-rapids.sh
-# don't enable rapids plugin by default
-sed -i '/spark.plugins=com.nvidia.spark.SQLPlugin/d' spark-rapids.sh
-gcloud storage cp spark-rapids.sh gs://${SPARK_DL_HOME}/init/
-# rm spark-rapids.sh
-
 # start cluster if not already running
-gcloud dataproc clusters list | grep "${cluster_name}"
-if [[ $? == 0 ]]; then
+if gcloud dataproc clusters list | grep -q "${cluster_name}"; then
     echo "Cluster ${cluster_name} already exists."
 else
     gcloud dataproc clusters create ${cluster_name} \
