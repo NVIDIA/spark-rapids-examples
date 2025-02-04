ae292de6e771e56aae718f6c68ffee7be88fcff8
Rishi C. <77904151+rishic3@users.noreply.github.com>
2025-02-04 09:20:15 -0800

    Support for Spark DL notebooks with PyTriton on Databricks/Dataproc (#483)
    ### Support for running DL Inference notebooks on CSP environments.
    - Refactored Triton sections to use PyTriton, a Python API for the
    Triton inference server which avoids Docker. Once this PR is merged,
    Triton sections no longer need to be skipped in the CI pipeline
    @YanxuanLiu .
    - Updated notebooks with instructions to run on Databricks/Dataproc
    - Updated Torch notebooks with best practices for ahead-of-time TensorRT
    compilation.
    - Cleaned up README, removing instructions to start Jupyter with PySpark
    (we need a cell to attach to standalone for CI/CD anyway, so hoping to
    reduce confusion for user).
    
    Notebook outputs are saved from running locally, but all notebooks were
    tested on Databricks/Dataproc.
    
    ---------
    
    Signed-off-by: Rishi Chandra <rishic@nvidia.com>

---
+++ examples/ML+DL-Examples/Spark-DL/dl_inference/dataproc/README.md
@@ -0,0 +1,70 @@
+# Spark DL Inference on Dataproc
+
+## Setup
+
+**Note**: fields in \<brackets\> require user inputs.
+
+#### Setup GCloud CLI
+
+1. Install the latest [gcloud-cli](https://cloud.google.com/sdk/docs/install) and initialize with `gcloud init`.
+
+2. Configure the following settings:
+    ```shell
+    export PROJECT=<your_project>
+    export DATAPROC_REGION=<your_dataproc_region>
+    export COMPUTE_REGION=<your_compute_region>
+    export COMPUTE_ZONE=<your_compute_zone>
+
+    gcloud config set project ${PROJECT}
+    gcloud config set dataproc/region ${DATAPROC_REGION}
+    gcloud config set compute/region ${COMPUTE_REGION}
+    gcloud config set compute/zone ${COMPUTE_ZONE}
+    ```
+
+#### Copy files to GCS
+
+3. Create a GCS bucket if you don't already have one:
+    ```shell
+    export GCS_BUCKET=<your_gcs_bucket_name>
+
+    gcloud storage buckets create gs://${GCS_BUCKET} 
+    ```
+
+4.  Specify the local path to the notebook(s) and copy to the GCS bucket.
+    As an example for a torch notebook:
+    ```shell
+    export SPARK_DL_HOME=${GCS_BUCKET}/spark-dl
+    
+    gcloud storage cp </path/to/notebook_name_torch.ipynb> gs://${SPARK_DL_HOME}/notebooks/
+    ```
+    Repeat this step for any notebooks you wish to run. All notebooks under `gs://${SPARK_DL_HOME}/notebooks/` will be copied to the master node during initialization.
+
+5. Copy the utils file to the GCS bucket.
+    ```shell
+    gcloud storage cp </path/to/pytriton_utils.py> gs://${SPARK_DL_HOME}/
+    ```
+
+#### Start cluster and run
+
+5. Specify the framework to use (torch or tf), which will determine what libraries to install on the cluster. For example:
+    ```shell
+    export FRAMEWORK=torch
+    ```
+    Run the cluster startup script. The script will also retrieve and use the [spark-rapids initialization script](https://github.com/GoogleCloudDataproc/initialization-actions/blob/master/spark-rapids/spark-rapids.sh) to setup GPU resources.
+    ```shell
+    cd setup
+    chmod +x start_cluster.sh
+    ./start_cluster.sh
+    ```
+    By default, the script creates a 4 node GPU cluster named `${USER}-spark-dl-inference-${FRAMEWORK}`.
+
+7. Browse to the Jupyter web UI:
+    - Go to `Dataproc` > `Clusters` > `(Cluster Name)` > `Web Interfaces` > `Jupyter/Lab`
+    
+    Or, get the link by running this command (under httpPorts > Jupyter/Lab):
+    ```shell
+    gcloud dataproc clusters describe ${CLUSTER_NAME} --region=${COMPUTE_REGION}
+    ```
+
+8. Open and run the notebook interactively with the **Python 3 kernel**.  
+The notebooks can be found under `Local Disk/spark-dl-notebooks` on the master node (folder icon on the top left > Local Disk).
---
+++ examples/ML+DL-Examples/Spark-DL/dl_inference/dataproc/setup/start_cluster.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -eo pipefail
+
+# configure arguments
+if [[ -z ${GCS_BUCKET} ]]; then
+    echo "Please export GCS_BUCKET per README.md"
+    exit 1
+fi
+
+if [[ -z ${FRAMEWORK} ]]; then
+    echo "Please export FRAMEWORK as 'torch' or 'tf'"
+    exit 1
+fi
+
+if [[ -z ${COMPUTE_REGION} ]]; then
+    COMPUTE_REGION=$(gcloud config get-value compute/region)
+    if [[ -z ${COMPUTE_REGION} ]]; then
+        echo "Please export COMPUTE_REGION per README.md or set it in gcloud config."
+        exit 1
+    fi
+fi
+
+SPARK_DL_HOME=${SPARK_DL_HOME:-${GCS_BUCKET}/spark-dl}
+
+# copy init script to gcs
+gcloud storage cp init_spark_dl.sh gs://${SPARK_DL_HOME}/init/
+INIT_PATH=gs://${SPARK_DL_HOME}/init/init_spark_dl.sh
+
+# retrieve and upload spark-rapids initialization script to gcs
+curl -LO https://raw.githubusercontent.com/GoogleCloudDataproc/initialization-actions/master/spark-rapids/spark-rapids.sh
+# don't enable rapids plugin by default
+sed -i '/spark.plugins=com.nvidia.spark.SQLPlugin/d' spark-rapids.sh
+gcloud storage cp spark-rapids.sh gs://${SPARK_DL_HOME}/init/
+# rm spark-rapids.sh
+
+COMMON_REQUIREMENTS="numpy
+pandas
+matplotlib
+portalocker
+pyarrow
+pydot
+scikit-learn
+huggingface
+datasets==3.*
+transformers
+urllib3<2
+nvidia-pytriton"
+
+TORCH_REQUIREMENTS="${COMMON_REQUIREMENTS}
+torch
+torchvision --extra-index-url https://download.pytorch.org/whl/cu121
+torch-tensorrt
+tensorrt --extra-index-url https://download.pytorch.org/whl/cu121
+sentence_transformers
+sentencepiece
+nvidia-modelopt[all] --extra-index-url https://pypi.nvidia.com"
+
+TF_REQUIREMENTS="${COMMON_REQUIREMENTS}
+tensorflow[and-cuda]
+tf-keras"
+
+cluster_name=${USER}-spark-dl-inference-${FRAMEWORK}
+if [[ ${FRAMEWORK} == "torch" ]]; then
+    requirements=${TORCH_REQUIREMENTS}
+    echo "========================================================="
+    echo "Starting PyTorch cluster ${cluster_name}"
+    echo "========================================================="
+elif [[ ${FRAMEWORK} == "tf" ]]; then
+    requirements=${TF_REQUIREMENTS}
+    echo "========================================================="
+    echo "Starting Tensorflow cluster ${cluster_name}"
+    echo "========================================================="
+else
+    echo "Please export FRAMEWORK as torch or tf"
+    exit 1
+fi
+
+# start cluster if not already running
+if gcloud dataproc clusters list | grep -q "${cluster_name}"; then
+    echo "Cluster ${cluster_name} already exists."
+else
+    gcloud dataproc clusters create ${cluster_name} \
+    --image-version=2.2-ubuntu \
+    --region ${COMPUTE_REGION} \
+    --master-machine-type n1-standard-16 \
+    --num-workers 4 \
+    --worker-min-cpu-platform="Intel Skylake" \
+    --worker-machine-type n1-standard-16 \
+    --master-accelerator type=nvidia-tesla-t4,count=1 \
+    --worker-accelerator type=nvidia-tesla-t4,count=1 \
+    --initialization-actions gs://${SPARK_DL_HOME}/init/spark-rapids.sh,${INIT_PATH} \
+    --metadata gpu-driver-provider="NVIDIA" \
+    --metadata gcs-bucket=${GCS_BUCKET} \
+    --metadata spark-dl-home=${SPARK_DL_HOME} \
+    --metadata requirements="${requirements}" \
+    --worker-local-ssd-interface=NVME \
+    --optional-components=JUPYTER \
+    --bucket ${GCS_BUCKET} \
+    --enable-component-gateway \
+    --max-idle "60m" \
+    --subnet=default \
+    --no-shielded-secure-boot
+fi
