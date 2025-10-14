1f90c81632bb60c6a77a93dbb45b42492ebe1413
Gera Shegalov <gshegalov@nvidia.com>
2025-10-13 22:00:09 -0700

    Add SOCKS5 proxy service to Docker Compose and update environment configuration
    - Introduced a SOCKS5 proxy service in docker-compose.yaml for remote access.
    - Added USE_LOCALHOST_UI environment variable to support localhost UI configuration.
    - Updated Dockerfile to copy spark-env.sh for environment variable management in the Spark worker.

--- examples/spark-connect-for-etl-and-ml/spark-connect-server/spark-env.sh
+++ examples/spark-connect-for-etl-and-ml/spark-connect-server/spark-env.sh
@@ -16,18 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# If SPARK_PUBLIC_DNS is empty, unset it
 if [[ "${USE_LOCALHOST_UI}" == "1" ]]; then
   export SPARK_PUBLIC_DNS=localhost
 fi
-
-GPU_COUNT_MAX=$(nvidia-smi -L | wc -l)
-export SPARK_WORKER_OPTS="
-  -Dspark.worker.resource.gpu.amount=${GPU_COUNT_MAX}
-  -Dspark.worker.resource.gpu.discoveryScript=/opt/spark/examples/src/main/scripts/getGpusResources.sh
-"
-
-# workaround for wheels installation not setting the correct LD_LIBRARY_PATH
-# https://github.com/rapidsai/cuml/issues/5300#issuecomment-2084646729
-LD_LIBRARY_PATH=$(find /usr/local/lib/python3.10/dist-packages/nvidia -name lib -type d | xargs printf '%s:'):$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
